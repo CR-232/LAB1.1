51c6b77b205d01aec89c8ac35af699c26800c08d
Admin <dcocieru2004@gmail.com>
2025-10-08 17:44:14 +0300

    Test

---
+++ Main/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Test GitHub Java Project ===");
+        System.out.println("Salut din IntelliJ!");
+        System.out.println("Dacă vezi acest mesaj pe GitHub, conexiunea funcționează corect ✅");
+    }
+}
