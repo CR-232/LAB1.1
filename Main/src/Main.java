0f07841c2388a0b9d7ceaf0ddedd54ed1abb2660
AngleofDark <dcocieru2004@gmail.com>
2025-10-30 14:27:40 +0200

    Lab2

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,55 +1,55 @@
 public class Main {
+    static class MyThread extends Thread {
+        public MyThread(ThreadGroup group, String name, int priority) {
+            super(group, name);
+            this.setPriority(priority);
+        }
+        public void run() {
+
+            System.out.println(
+                    "Thread: " + getName() +
+                    " | Group: " + getThreadGroup().getName() +
+                    " | Priority: " + getPriority());
+        }
+    }
+
     public static void main(String[] args) {
-        //Cocieru
+
         ThreadGroup sys = Thread.currentThread().getThreadGroup();
 
-        Thread curr = Thread.currentThread();
-        curr.setPriority(curr.getPriority()-2);
-
-        Thread Th1 = new Thread(curr, "Th1");
-        Th1.setPriority(7);
-        Thread Th2 = new Thread(curr, "Th2");
-        Th2.setPriority(7);
-        Thread ThA = new Thread(curr, "ThA");
-        ThA.setPriority(3);
+        MyThread Th1 = new MyThread(sys, "Th1", 7);
+        MyThread Th2 = new MyThread(sys, "Th2", 7);
+        MyThread ThA = new MyThread(sys, "ThA", 3);
 
         Th1.start();
         Th2.start();
         ThA.start();
-        sys.list();
 
-        ThreadGroup g1 = new ThreadGroup("G1");
-        g1.setMaxPriority(Thread.MAX_PRIORITY);
+        ThreadGroup g1 = new ThreadGroup(sys, "G1");
+
         ThreadGroup g3 = new ThreadGroup(g1, "G3");
-        g3.setMaxPriority(Thread.MAX_PRIORITY);
 
-        Thread Tha = new Thread(g3, "Tha");
-        Tha.setPriority(3);
+        MyThread Tha = new MyThread(g3, "Tha", 3);
+        MyThread Thb = new MyThread(g3, "Thb", 3);
+        MyThread Thc = new MyThread(g3, "Thc", 3);
+        MyThread Thd = new MyThread(g3, "Thd", 3);
+
         Tha.start();
-        Thread Thb = new Thread(g3, "Thb");
-        Thb.setPriority(3);
         Thb.start();
-        Thread Thc = new Thread(g3, "Thc");
-        Thc.setPriority(3);
         Thc.start();
-        Thread Thd = new Thread(g3, "Thd");
-        Thd.setPriority(3);
         Thd.start();
-        g3.list();
 
-        ThreadGroup g2 = new ThreadGroup("G2");
-        g2.setMaxPriority(Thread.MAX_PRIORITY);
+        ThreadGroup g2 = new ThreadGroup(sys, "G2");
 
-        Thread Th1_g2 = new Thread(g2, "Th1");
+        MyThread Th1_g2 = new MyThread(g2, "Th1", 4);
+        MyThread Th2_g2 = new MyThread(g2, "Th2", 5);
+        MyThread Th3_g2 = new MyThread(g2, "Th3", 5);
+
         Th1_g2.start();
-        Th1_g2.setPriority(4);
-        Thread Th2_g2 = new Thread(g2, "Th2");
         Th2_g2.start();
-        Th2_g2.setPriority(5);
-        Thread Th3_g2 = new Thread(g2, "Th3");
         Th3_g2.start();
-        Th3_g2.setPriority(5);
-        g2.list();
 
+        System.out.println("\n Structura ThreadGroup");
+        sys.list();
     }
 }
